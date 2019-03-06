ba15039dd66999c8d5a1b0b0d203dfc8c681023b
jaeho <ovsoce@gmail.com>
2019-03-07 07:14:05 +0900

    ViewPager2 -> ViewPager

--- app/src/main/java/io/github/ovso/systemreport/viewmodels/MainViewModel.kt
+++ app/src/main/java/io/github/ovso/systemreport/viewmodels/MainViewModel.kt
@@ -2,24 +2,12 @@ package io.github.ovso.systemreport.viewmodels
 
 import android.app.Application
 import androidx.databinding.ObservableField
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
-import io.github.ovso.systemreport.R
-import io.github.ovso.systemreport.utils.ResourceProvider
 import io.github.ovso.systemreport.view.ui.main.SimpleOnTabSelectedListener
 
 class MainViewModel constructor(private var application: Application) : ViewModel() {
-  var itemsLiveData = MutableLiveData<ArrayList<String>>()
-  var titlesLiveData = MutableLiveData<ArrayList<String>>()
   var selectedObField = ObservableField(0)
-  var resProvider: ResourceProvider = ResourceProvider(application.applicationContext)
-  fun fetchList() {
-    var items = resProvider.getStringArray(R.array.info_type)
-        .toCollection(ArrayList())
-    itemsLiveData.value = items
-    titlesLiveData.value = items
-  }
 
   var onPageChangeCallback = object : OnPageChangeCallback() {
     override fun onPageSelected(position: Int) {
