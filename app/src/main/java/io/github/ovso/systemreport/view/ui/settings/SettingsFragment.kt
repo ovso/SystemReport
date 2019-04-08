ecfb93affc23864d8370b35bc80ace6e6cbb3751
jaeho <ovsoce@gmail.com>
2019-04-08 22:34:55 +0900

    Add license dialog

--- app/src/main/java/io/github/ovso/systemreport/view/ui/settings/SettingsFragment.kt
+++ app/src/main/java/io/github/ovso/systemreport/view/ui/settings/SettingsFragment.kt
@@ -2,19 +2,43 @@ package io.github.ovso.systemreport.view.ui.settings
 
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.preference.ListPreference
+import androidx.preference.Preference
+import androidx.preference.Preference.OnPreferenceClickListener
 import androidx.preference.PreferenceFragmentCompat
+import de.psdev.licensesdialog.LicensesDialog
 import io.github.ovso.systemreport.R
 
-class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
+class SettingsFragment : PreferenceFragmentCompat(),
+    SharedPreferences.OnSharedPreferenceChangeListener, OnPreferenceClickListener {
+  override fun onPreferenceClick(preference: Preference?): Boolean {
+    LicensesDialog.Builder(activity!!)
+        .setNotices(io.github.ovso.systemreport.R.raw.notices)
+        .build()
+        .show()
+    return true
+  }
+
+  private lateinit var tempUnitPreference: ListPreference
 
   companion object {
     fun newInstance() = SettingsFragment()
+    const val KEY_TEMPERATURE_UNIT = "temperature_unit"
+    const val KEY_OPENSOURCE = "opensource"
   }
 
   override fun onSharedPreferenceChanged(
     sharedPreferences: SharedPreferences?,
     key: String?
   ) {
+    when (key) {
+      KEY_TEMPERATURE_UNIT ->
+        tempUnitPreference.summary = tempUnitPreference.entry.toString()
+    }
   }
 
   override fun onCreatePreferences(
@@ -22,5 +46,29 @@ class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedP
     rootKey: String?
   ) {
     addPreferencesFromResource(R.xml.preferences)
+
+    tempUnitPreference = preferenceScreen.findPreference(KEY_TEMPERATURE_UNIT) as ListPreference
+    (preferenceScreen.findPreference(KEY_OPENSOURCE) as Preference).onPreferenceClickListener = this
+  }
+
+  override fun onResume() {
+    super.onResume()
+    tempUnitPreference.summary = tempUnitPreference.entry.toString()
+    preferenceScreen.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+  }
+
+  override fun onPause() {
+    super.onPause()
+    preferenceScreen.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+  }
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    val view = super.onCreateView(inflater, container, savedInstanceState)
+    view?.setBackgroundColor(ContextCompat.getColor(context!!, android.R.color.black))
+    return view
   }
 }
